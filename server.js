331fb5a075bb9bfda5a71e3c6a4441171bc1a7c8
Sanjay6383 <sanjayv.cse2023@citchennai.net>
2025-07-17 13:53:11 +0530

    files are added

--- server.js
+++ server.js
@@ -10,8 +10,12 @@ const laundryRoutes = require('./routes/laundry');
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/admin', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'admin.html'));
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
